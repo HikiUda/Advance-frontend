c649cd008e868a09d12a744e18cbcb693efe2061
HikiUda <urupindima4@gmail.com>
2025-02-28 10:27:12 +0800

    f

--- src/app/providers/router/ui/AppRouter.test.tsx
+++ src/app/providers/router/ui/AppRouter.test.tsx
@@ -60,7 +60,7 @@ describe('app/router/AppRouter', () => {
             },
         });
 
-        const page = await screen.findByTestId('AdminPanelPagee');
+        const page = await screen.findByTestId('AdminPanelPag');
         expect(page).toBeInTheDocument();
     });
 });
